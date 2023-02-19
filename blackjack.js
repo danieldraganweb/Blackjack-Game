52a89c912aa4fcb071a29b9ac3359c2bb026bacb
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-02-19 15:01:41 +0100

    stle btns and body

--- blackjack.js
+++ blackjack.js
@@ -3,70 +3,43 @@ let totalWins = document.querySelector("#wins");
 let totalLosses = document.querySelector("#losses");
 let bet = document.querySelector("#bet-el");
 let message = document.querySelector("#message-el");
-let dealerSum = document.querySelector("#dealer-sum");
-let playerSum = document.querySelector("#player-sum");
+let dealerSumEl = document.querySelector("#dealer-sum");
+let playerSumEl = document.querySelector("#player-sum");
 let playerCards = document.querySelector("#player-cards");
 let dealerCards = document.querySelector("#dealer-cards");
 let betInput = document.querySelector("#bet-input");
-let hasBlackJack = false;
 let hiddenCard = document.querySelector("#hidden-card");
+let hasBlackJack = false;
 let isAlive = true;
 let canHit = true;
+console.log(bet);
+console.log(balance);
+console.log(totalWins);
+console.log(totalLosses);
+console.log(message);
+console.log(dealerSumEl);
+console.log(playerSumEl);
+console.log(playerCards);
+console.log(dealerCards);
 console.log(betInput);
+console.log(hiddenCard);
+console.log(hasBlackJack);
+console.log(isAlive);
+console.log(canHit);
 
+function renderGame() {}
 function getRandomCard() {
-  let randomNumber = Math.floor(Math.random() * 13) + 1;
-  if (randomNumber > 10) {
-    return 10;
-  } else if (randomNumber === 1) {
-    return 11;
-  } else {
-    return randomNumber;
-  }
+  console.log("Button was clicked!");
 }
-window.onload = function () {
-  buildDeck();
-};
-function builtDeck() {
-  let deck = [];
-  let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-  let values = "2,3,4,5,6,7,8,9,10,J,Q,K,A";
-  for (let value of values.split(",")) {
-    for (let suit of suits) {
-      deck.push({ value, suit });
-    }
-  }
-  return deck;
+
+function startNewGame() {
+  console.log("Button was clicked!");
 }
-function shuffleDeck(deck) {
-  for (let i = 0; i < deck.length; i++) {
-    let swapIdx = Math.trunc(Math.random() * deck.length);
-    let tmp = deck[swapIdx];
-    deck[swapIdx] = deck[i];
-    deck[i] = tmp;
-  }
+
+function newCard() {
+  console.log("Button was clicked!");
 }
-let deck = builtDeck();
-// shuffleDeck(deck);
-// let playerCards = [getRandomCard(), getRandomCard()];
-// let dealerCards = [getRandomCard(), getRandomCard()];
-// let playerSum = 0;
-// let dealerSum = 0;
-// let message = "";
-// let hasBlackJack = false;
-// let isAlive = false;
-// let messageEl = document.getElementById("message-el");
-// let sumEl = document.getElementById("sum-el");
-// let cardsEl = document.getElementById("cards-el");
-// let playerEl = document.getElementById("player-el");
-// let dealerEl = document.getElementById("dealer-el");
-// let balanceEl = document.getElementById("balance-el");
-// function addPlayerBet() {
-//   console.log("Bet clicked");
-// }
-function drawNewCard() {
-  console.log(card);
-}
-function stopDrawing() {
-  console.log("Stop drawing");
+
+function stay() {
+  console.log("Button was clicked!");
 }
